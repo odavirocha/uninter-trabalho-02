4e17538794ab98785ba53b996912eda0c6047dd8
Davi Rocha <davirochatz@gmail.com>
2026-06-15 11:37:39 -0300

    feat: trabalho

---
+++ script/Projetos.js
@@ -0,0 +1,34 @@
+const pinneds = new Set([1115378144, 1113897194, 1110544851, 1158191786]) // projetos marcados no github
+
+// busca 100 repositórios do github
+async function getRepositories() {
+    try {
+        const response = await fetch("https://api.github.com/users/odavirocha/repos?per_page=100")
+
+        if (!response.ok) {
+            throw new Error("Erro de conexão com o github!")
+        }
+
+        return response.json()
+    } catch (error) {
+        console.error("Erro ao buscar os repositórios.")
+    }
+}
+
+// filtra os projetos pelos marcados
+async function getPinneds() {
+    let data = await getRepositories()
+    const result = data.filter(item => pinneds.has(item.id))
+    result.forEach((repository, index) => {
+        // cria uma guia com o repositório aberto
+        document.querySelector(`#card-projetos-0${index}`).href = `${repository.html_url}` 
+        document.querySelector(`#card-projetos-0${index}`).target = '_blank'
+        
+        // popula os campos do card
+        document.querySelector(`#card-projetos-0${index} .projetos-card-img`).src = `../assets/${repository.id}.png`
+        document.querySelector(`#name-projetos-card-0${index}`).textContent = repository.name
+        document.querySelector(`#description-projetos-card-0${index}`).textContent = repository.description
+    })
+}
+
+getPinneds()
---
+++ script/Validate.js
@@ -0,0 +1,78 @@
+let nameInput = document.querySelector(".contato-name-input")
+let emailInput = document.querySelector(".contato-email-input")
+let messageInput = document.querySelector(".contato-message-input")
+
+// limpa as validações
+nameInput.addEventListener("input", () => nameInput.setCustomValidity(""))
+emailInput.addEventListener("input", () => emailInput.setCustomValidity(""))
+messageInput.addEventListener("input", () => messageInput.setCustomValidity(""))
+
+function validateName() {
+    let name = nameInput.value.trim().split(/\s+/)
+
+    // verifica se não ta vazio
+    if (nameInput.value.length == 0) {
+        nameInput.setCustomValidity("Preencha seu nome e sobrenome!")
+        nameInput.reportValidity()
+        return false
+    }
+
+    // verifica se o array tem 2 valores, sendo nome e sobrenome
+    if ((name.length < 2)) {
+        nameInput.setCustomValidity("Digite seu nome e sobrenome!")
+        nameInput.reportValidity()
+        return false
+    }
+
+    return true
+}
+
+function validateEmail() {
+    let regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/ // regex que valida emails
+    
+    // verifica se não ta vazio
+    if (emailInput.value.length == 0) {
+        emailInput.setCustomValidity("Preencha seu e-mail!")
+        emailInput.reportValidity()
+        return false
+    }
+
+    // valida se o input passa na regex
+    if (!regex.test(emailInput.value)) {
+        emailInput.setCustomValidity("E-mail inválido!")
+        emailInput.reportValidity()
+        return false
+    }
+
+    return true
+}
+
+function validateMessage() {
+
+    // verifica se não ta vazio
+    if (messageInput.value.length == 0) {
+        messageInput.setCustomValidity("Escreva uma mensagem!")
+        messageInput.reportValidity()
+        return false
+    }
+
+    // verfica se a mensagem não é muito grande
+    if (messageInput.value.length > 300) {
+        messageInput.setCustomValidity("A mensagem deve ter no máximo 300 caracteres")
+        messageInput.reportValidity()
+        return false
+    }
+
+    return true
+}
+
+document.querySelector(".contato-form").addEventListener("submit", function(event) {
+    event.preventDefault()
+
+    // chama os validadores
+    if (!validateName()) return
+    if (!validateEmail()) return
+    if (!validateMessage()) return
+
+    alert("Mensagem enviada com sucesso!")
+})
